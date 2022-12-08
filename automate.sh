b269f0da083190b749908f44a0946b697549c146
Tran Xuan Loc <loc.tran@moreh.com.vn>
2022-12-08 19:20:41 +0900

    adjust automation script for HAC VM

--- automate.sh
+++ automate.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# Currently working on HAC machines only, A100 machines are not supported
+
 set -e
 
-DATASET_ROOT="/data/work/datasets"
+DATASET_ROOT="/NAS/common_data"
 LJSPEECH_LINK="https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2"
 ALIGNMENT_GGDRIVE_ID="1ukb8o-SnqhXCxq7drI3zye3tZdrGvQDA"
 
@@ -17,20 +19,32 @@ fi
 
 if [[ ! -d preprocessed_data/LJSpeech/TextGrid ]]; then
     mkdir -p preprocessed_data/LJSpeech
+    [[ ! -x "$(command -v gdown)" ]] && echo "gdown not found" && exit 1
     gdown https://drive.google.com/uc?id=$ALIGNMENT_GGDRIVE_ID \
         --output LJSpeech.zip
     unzip LJSpeech.zip -d preprocessed_data/LJSpeech
     rm -f LJSpeech.zip
 fi
 
+corpus_path=$(grep corpus_path: config/LJSpeech/preprocess.yaml | awk '{print $NF}' | tr -d '"')
+[[ ! -d $corpus_path ]] && echo "${corpus_path} in preprocess.yaml not exist" && exit 1
+
 env_file="hacenv.yml"
 env_name=$(grep ^name: $env_file | awk '{print $NF}')
 
 conda env create -f $env_file
-# conda env run -n $env_name update-moreh --force
+conda run -n $env_name update-moreh --force
 
+echo "Running prepare_align.py"
 conda run -n $env_name python3 prepare_align.py config/LJSpeech/preprocess.yaml
+
+echo "Running preprocess.py"
 conda run -n $env_name python3 preprocess.py config/LJSpeech/preprocess.yaml
+
+train_steps=$(grep total_step: config/LJSpeech/train.yaml | awk '{print $NF}')
+[[ $train_step -gt 10000 ]] && echo "Total train steps ${train_steps} too big" && exit 1
+
+echo "Running train.py"
 conda run -n $env_name python3 train.py \
     -p config/LJSpeech/preprocess.yaml \
     -m config/LJSpeech/model.yaml \
